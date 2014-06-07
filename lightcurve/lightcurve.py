e908f8caee84a8c7984aa7e412532fdfd64160bf
Daniel Williams <mail@daniel-williams.co.uk>
2014-06-07 23:35:03 +0100

    Added the very barebones of the Lightcurve class.

--- lightcurve/lightcurve.py
+++ lightcurve/lightcurve.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+class Lightcurve():
+    """
+    This class is designed to handle light curves; time series of
+    electromagnetic data, frequently encountered in astronomical
+    data.
+    """
+
+    def __init__():
+
+        
