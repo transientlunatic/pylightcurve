23241e555fed72b3d9c34a12b6dfcf4a630948cb
Daniel Williams <mail@daniel-williams.co.uk>
2014-06-24 16:25:12 +0100

    Added the ability for the Lightcurve class to store and return header metadata.

--- lightcurve/lightcurve.py
+++ lightcurve/lightcurve.py
@@ -15,23 +15,33 @@ class Lightcurve():
     """
 
     data = pd.DataFrame()
+    meta = {}
     title = "Light Curve"
 
-    def __init__(self, title="Light Curve"):
+    def __init__(self, start, end):
         """
         Assemble the Lightcurve.
         
         """
-        self.title = title
+        pass
     
-    def import_data(self, data):
+    def import_data(self, data_list):
+        data = data_list[0]
+        meta = data_list[1]
         self.data = self.data.join(data, how="outer")
+        for column in data.columns.values.tolist():
+            self.meta[column] = meta[column]
+        
         return self
+    
+    def header(self):
+        for column in self.meta:
+            print column
+            for field in self.meta[column]:
+                print "\t"+str(field)+":\t"+str(self.meta[column][field])
+            print "\n"
     
     def plot(self):
         self.data = self.data.fillna(method='pad')
         self.data.neutron_flux.plot(figsize=(18,6))
         self.data.sunspot.plot(secondary_y=True)
-        
-        
-        
