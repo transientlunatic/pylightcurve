dc2c26ace99c24d9ae477029f5187a7e3a664e5d
Daniel Williams <mail@daniel-williams.co.uk>
2014-07-02 15:28:54 +0100

    Completed the majority of the Lightcurve class.

--- lightcurve/__init__.py
+++ lightcurve/__init__.py
@@ -2,4 +2,7 @@
 
 __author__ = 'Daniel Williams'
 __email__ = 'mail@daniel-williams.co.uk'
-__version__ = '0.1.0'
+__version__ = '0.0.0'
+
+#from .filters.savitzkygolay import savitzky_golay
+from lightcurve import *
